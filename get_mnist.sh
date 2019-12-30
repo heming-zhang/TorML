fb2482e6f3e6d4385c02eae3f9e9e30ac8e158a6
Heming <im.hemingzhang@gmail.com>
2019-12-29 22:33:11 -0600

    init

---
+++ get_mnist.sh
@@ -0,0 +1,20 @@
+if [ -d MNIST ]; then
+    echo "MNIST directory already exists, exiting."
+    exit 1
+fi
+
+mkdir MNIST
+wget -P ./MNIST/ http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz \
+http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz \
+http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz \
+http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz \
+
+pushd MNIST
+gunzip *
+popd
+
+mkdir MNIST_BIN
+hexdump -C ./MNIST/train-images-idx3-ubyte >./MNIST_BIN/trainimages
+hexdump -C ./MNIST/train-labels-idx1-ubyte >./MNIST_BIN/trainlabels
+hexdump -C ./MNIST/t10k-images-idx3-ubyte >./MNIST_BIN/testimages
+hexdump -C ./MNIST/t10k-labels-idx1-ubyte >./MNIST_BINtestlabels
