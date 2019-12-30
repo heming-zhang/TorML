0e6c67fe2228e0b95699a65c59e5a43622ff7693
Heming <im.hemingzhang@gmail.com>
2019-12-29 23:37:33 -0600

    modify get_mnist

--- get_mnist.sh
+++ get_mnist.sh
@@ -3,6 +3,11 @@ if [ -d MNIST ]; then
     exit 1
 fi
 
+if [ -d MNIST_BIN ]; then
+    echo "MNIST_BIN directory already exists, exiting."
+    exit 1
+fi
+
 mkdir MNIST
 wget -P ./MNIST/ http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz \
 http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz \
@@ -17,4 +22,4 @@ mkdir MNIST_BIN
 hexdump -C ./MNIST/train-images-idx3-ubyte >./MNIST_BIN/trainimages
 hexdump -C ./MNIST/train-labels-idx1-ubyte >./MNIST_BIN/trainlabels
 hexdump -C ./MNIST/t10k-images-idx3-ubyte >./MNIST_BIN/testimages
-hexdump -C ./MNIST/t10k-labels-idx1-ubyte >./MNIST_BINtestlabels
+hexdump -C ./MNIST/t10k-labels-idx1-ubyte >./MNIST_BIN/testlabels
