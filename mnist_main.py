60aecd00987dcc2ef69849c3ae5a80c459cd7d54
Heming <im.hemingzhang@gmail.com>
2020-01-04 02:48:25 -0600

    finish mnist_nn;

--- mnist_main.py
+++ mnist_main.py
@@ -1,5 +1,8 @@
 import os
 from mnist_parse import ParseFile
+from mnist_nn import RunMnistNN
+from mnist_nn import MnistNNLoad
+from mnist_nn import MnistParameter
 
 def parse_file():
     train_img = "./MNIST/train-images-idx3-ubyte"
@@ -20,8 +23,36 @@ def classify_file():
         ParseFile(train_img, train_label, i).classify()
         ParseFile(test_img, test_label, i).classify()
 
+def plot(epoch, train_loss, test_loss):
+    return 0
+
 if __name__ == "__main__":
     if os.path.isdir("./mnist") == False:
         parse_file()
     if os.path.isdir("./mnist_classified") == False:
         classify_file()
+    # Load data with mini-batch
+    nn_batchsize = 640
+    nn_worker = 2
+    train_loader, test_loader = MnistNNLoad(nn_batchsize, 
+                nn_worker).load_data()
+    # Use Neural Network as training model
+    nn_learning_rate = 0.01
+    nn_momentum = 0.5
+    model, criterion, optimizer = MnistParameter(
+                nn_learning_rate,
+                nn_momentum).mnist_function()
+    # Get experimental results in every epoch
+    nn_epoch_num = 20
+    for epoch in range(1 : nn_epoch_num + 1):
+        train_loss = RunMnistNN(model,
+                    criterion,
+                    optimizer,
+                    train_loader,
+                    test_loader).train_mnist_nn()
+        test_loss = RunMnistNN(model,
+                    criterion,
+                    optimizer,
+                    train_loader,
+                    test_loader).test_mnist_nn()
+        print(epoch, train_loss, test_loss)
--- mnist_nn.py
+++ mnist_nn.py
@@ -7,15 +7,15 @@ from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from mnist_nn_dataset import MnistNNTrain, MnistNNTest
 
-class MnistLoad():
+class MnistNNLoad():
     def __init__(self, batchsize, worker):
         self.batchsize = batchsize
         self.worker = worker
 
     # Design a mini-batch gradient descent
     def load_data(self):
-        batch_size = self.batch_size
-        num_worker = self.num_worker
+        batchsize = self.batchsize
+        worker = self.worker
         train_dataset = MnistNNTrain()
         train_loader = DataLoader(dataset = train_dataset,
                     batch_size = batchsize,
@@ -46,32 +46,70 @@ class MnistNN(nn.Module):
         y_pred = self.l5(out4)
         return y_pred
 
-class RunMnistNN():
+class MnistParameter():
     def __init__(self, 
                 learning_rate, 
-                momentum, 
-                epoch_num,
-                batchsize,
-                worker):
+                momentum):
         self.learning_rate = learning_rate
         self.momentum = momentum
-        self.epoch_num = epoch_num
-        self.bacthsize = batchsize
-        self.worker = worker
     
-    def train_mnist_nn(self):
+    def mnist_function(self):
         learning_rate = self.learning_rate
         momentum = self.momentum
-        epoch_num = self.epoch_num
         model = MnistNN()
-        criterion = torch.nn.CrossEntropyLoss()
+        criterion = nn.CrossEntropyLoss()
         optimizer = torch.optim.SGD(model.parameters(),
                     lr = learning_rate, 
                     momentum = momentum)
+        return model, criterion, optimizer
 
-        batchsize = self.bacthsize
-        worker = self.worker
-        train_loader, test_loader = MnistLoad
-        for epoch in range(1, epoch_num + 1):
-            for i, data in enumerate()
+class RunMnistNN():
+    def __init__(self, model, 
+                criterion, optimizer, 
+                train_loader, test_loader):
+        self.model = model
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.train_loader = train_loader
+        self.test_loader = test_loader
 
+    def train_mnist_nn(self):
+        model = self.model
+        model.train()
+        criterion = self.criterion
+        optimizer = self.optimizer
+        train_loader = self.train_loader
+        # train data in certain epoch
+        for i, data in enumerate(train_loader):
+            train_input, train_label = data
+            train_input = np.array(train_input)
+            train_label = np.array(train_label)
+            # wrap them in Variable
+            train_input = Variable(torch.Tensor(train_input), requires_grad = False)
+            train_label = Variable(torch.LongTensor(train_label), requires_grad = False)
+            # Forward pass: Compute predicted y by passing x to the model
+            y_pred = model(train_input)
+            # Compute the train_loss with Cross-Entropy loss
+            train_loss = criterion(y_pred, train_label)
+            # Clear gradients of all optimized class
+            optimizer.zero_grad()
+            train_loss.backward()
+            optimizer.step()
+        return float(train_loss)
+    
+    def test_mnist_nn(self):
+        model = self.model
+        model.eval()
+        criterion = self.criterion
+        optimizer = self.optimizer
+        test_loader = self.test_loader
+        for i, data in enumerate(test_loader):
+            test_input, test_label = data
+            test_input = np.array(test_input)
+            test_label = np.array(test_label)
+            test_input = Variable(torch.Tensor(test_input), requires_grad = False)
+            test_label = Variable(torch.LongTensor(test_label), requires_grad = False)
+            # Forward pass: Compute predicted y by passing x to the model
+            y_pred = model(test_input)
+            test_loss = criterion(y_pred, test_label)
+        return float(test_loss)
